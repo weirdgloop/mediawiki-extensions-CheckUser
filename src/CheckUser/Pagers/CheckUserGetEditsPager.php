weirdgloop/mediawiki-extensions-CheckUser

commit 022e4ffc915af7ab39a4cee534595b9d4b9d6018

Implement read new support for CheckUserGetEditsPager
Why:
* Special:CheckUser's 'Get edits' mode needs to read from the
  three "result tables" (cu_changes, cu_log_event, and
  cu_private_event) when reading new for event table migration.
* Using the union query builder for large queries took too
  long to return on production wikis when testing using
  an example query. However, three separate queries were
  faster.
** Previously, the CheckUserUnionSelectQueryBuilder was going
   to be used to generate these queries and this was added in
   00e0686f.
** The time differences were clearly seen at 5,000 results where
   a UNION query took 32 seconds on the English Wikipedia whereas
   three separate queries took combined under a second to run.

What:
* Modify AbstractCheckUserPager::getIpConds to return conditions
  that are specific to a table by passing the table name
** Add the ::getIpHexColumn method to achieve this
* Add AbstractCheckUserPager::getIndexName to return the database
  index name for a given table and value in $this->xfor.
* Update AbstractCheckUserPager::getIndexField and ::getTimestamp
  Field to reflect the changes to using three queries
** Make ::getTimestampField optionally take a table name. If no
   table name is provided, return "timestamp". Otherwise return
   the timestamp column name specific to the table provided in
   the table name argument.
* Re-define the abstract ::getQueryInfo in AbstractCheckUserPager
  to take a table name. This is optional in the arguments, but
  not passing a table name when calling the method will throw a
  LogicException. All calls to this method in code that extends
  or is in AbstractCheckUserPager have been updated to pass a
  table name.
* Override ::buildQueryInfo in AbstractCheckUserPager to pass
  a table name to ::getQueryInfo, generate query info for the
  three tables if reading new.
** When reading old it returns just the query info for cu_changes

src/CheckUser/Pagers/CheckUserGetEditsPager.php
@@ -8,6 +8,9 @@ use Html;
 use HtmlArmor;
 use IContextSource;
 use Linker;
+use LogFormatter;
+use LogicException;
+use ManualLogEntry;
 use MediaWiki\Cache\LinkBatchFactory;
 use MediaWiki\CheckUser\CheckUser\SpecialCheckUser;
 use MediaWiki\CheckUser\Hook\HookRunner;
@@ -180,13 +183,30 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 			// Add any block information
 			$templateParams['flags'] = $this->flagCache[$row->user_text];
 		}
-		// Action text, hackish ...
-		$templateParams['actionText'] = $this->commentFormatter->format( $row->actiontext );
+		if ( $this->eventTableReadNew && $row->type == RC_LOG && isset( $row->log_type ) && $row->log_type ) {
+			// Log action text taken from the LogFormatter for the entry being displayed.
+			$logEntry = new ManualLogEntry( $row->log_type, $row->log_action );
+			if ( $row->log_params !== null ) {
+				$logEntry->setParameters( ManualLogEntry::extractParams( $row->log_params ) );
+			}
+			$logEntry->setPerformer( $user );
+			if ( $row->title ) {
+				$logEntry->setTarget( Title::makeTitle( $row->namespace, $row->title ) );
+			} elseif ( $row->page_id ) {
+				$logEntry->setTarget( Title::newFromID( $row->page_id ) );
+			}
+			$logEntry->setTimestamp( $row->timestamp );
+			$logFormatter = LogFormatter::newFromEntry( $logEntry );
+			$templateParams['actionText'] = $logFormatter->getActionText();
+		} else {
+			// Action text, hackish ...
+			$templateParams['actionText'] = $this->commentFormatter->format( $row->actiontext ?? '' );
+		}
 		// Comment
 		if ( $row->type == RC_EDIT || $row->type == RC_NEW ) {
 			$templateParams['comment'] = $this->formattedRevisionComments[$row->this_oldid];
 		} else {
-			$comment = $this->commentStore->getComment( 'cuc_comment', $row );
+			$comment = $this->commentStore->getComment( 'comment', $row );
 			$templateParams['comment'] = $this->commentFormatter->formatBlock( $comment->text );
 		}
 		// IP
@@ -226,15 +246,29 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 		// Due to T315224 triple equals for type does not work for sqlite.
 		if ( $row->type == RC_LOG ) {
 			$title = Title::makeTitle( $row->namespace, $row->title );
-			$links['log'] = Html::rawElement(
-				'span',
-				[ 'class' => 'mw-changeslist-links' ],
-				$this->getLinkRenderer()->makeKnownLink(
+			$links['log'] = '';
+			if ( $this->eventTableReadNew && isset( $row->log_id ) ) {
+				$links['log'] = Html::rawElement( 'span', [],
+					$this->getLinkRenderer()->makeKnownLink(
+						SpecialPage::getTitleFor( 'Log' ),
+						new HtmlArmor( $this->message['checkuser-log-link-text'] ),
+						[],
+						[ 'logid' => $row->log_id ]
+					)
+				) . ' ';
+			}
+			$links['log'] .= Html::rawElement( 'span', [],
+					$this->getLinkRenderer()->makeKnownLink(
 					SpecialPage::getTitleFor( 'Log' ),
-					new HtmlArmor( $this->message['log'] ),
+					new HtmlArmor( $this->message['checkuser-logs-link-text'] ),
 					[],
 					[ 'page' => $title->getPrefixedText() ]
 				)
+			);
+			$links['log'] = Html::rawElement(
+				'span',
+				[ 'class' => 'mw-changeslist-links' ],
+				$links['log']
 			);
 		} else {
 			$title = Title::makeTitle( $row->namespace, $row->title );
@@ -314,7 +348,10 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 	 */
 	protected function preCacheMessages() {
 		if ( $this->message === [] ) {
-			$msgKeys = [ 'diff', 'hist', 'minoreditletter', 'newpageletter', 'blocklink', 'log' ];
+			$msgKeys = [
+				'diff', 'hist', 'minoreditletter', 'newpageletter',
+				'blocklink', 'checkuser-log-link-text', 'checkuser-logs-link-text'
+			];
 			foreach ( $msgKeys as $msg ) {
 				$this->message[$msg] = $this->msg( $msg )->escaped();
 			}
@@ -322,9 +359,38 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 	}
 
 	/** @inheritDoc */
-	public function getQueryInfo(): array {
-		$commentQuery = $this->commentStore->getJoin( 'cuc_comment' );
+	public function getQueryInfo( ?string $table = null ): array {
+		if ( $table === null ) {
+			throw new LogicException(
+				"This ::getQueryInfo method must be provided with the table to generate " .
+				"the correct query info"
+			);
+		}
 
+		if ( $table === self::CHANGES_TABLE ) {
+			$queryInfo = $this->getQueryInfoForCuChanges();
+		} elseif ( $table === self::LOG_EVENT_TABLE ) {
+			$queryInfo = $this->getQueryInfoForCuLogEvent();
+		} elseif ( $table === self::PRIVATE_LOG_EVENT_TABLE ) {
+			$queryInfo = $this->getQueryInfoForCuPrivateEvent();
+		}
+
+		$queryInfo['options']['USE INDEX'] = [ $table => $this->getIndexName( $table ) ];
+
+		if ( $this->xfor === null ) {
+			$queryInfo['conds']['actor_user'] = $this->target->getId();
+		} else {
+			$ipConds = self::getIpConds( $this->mDb, $this->target->getName(), $this->xfor, $table );
+			if ( $ipConds ) {
+				$queryInfo['conds'] = array_merge( $queryInfo['conds'], $ipConds );
+			}
+		}
+		return $queryInfo;
+	}
+
+	/** @inheritDoc */
+	protected function getQueryInfoForCuChanges(): array {
+		$commentQuery = $this->commentStore->getJoin( 'cuc_comment' );
 		$queryInfo = [
 			'fields' => [
 				'namespace' => 'cuc_namespace',
@@ -339,11 +405,12 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 				'ip' => 'cuc_ip',
 				'xff' => 'cuc_xff',
 				'agent' => 'cuc_agent',
-				'user' => 'actor_cuc_user.actor_user',
-				'user_text' => 'actor_cuc_user.actor_name',
-				# Needed for IndexPager
-				'cuc_timestamp'
-			] + $commentQuery['fields'],
+				'user' => 'actor_user',
+				'user_text' => 'actor_name',
+				# Needed for rows with cuc_type as RC_LOG.
+				'comment_text',
+				'comment_data',
+			],
 			'tables' => [ 'cu_changes', 'actor_cuc_user' => 'actor' ] + $commentQuery['tables'],
 			'conds' => [],
 			'join_conds' => [
@@ -351,17 +418,102 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 			] + $commentQuery['joins'],
 			'options' => [],
 		];
-		if ( $this->xfor === null ) {
-			$queryInfo['conds']['actor_user'] = $this->target->getId();
-			$queryInfo['options']['USE INDEX'] = [ 'cu_changes' => 'cuc_actor_ip_time' ];
-		} else {
-			$queryInfo['options']['USE INDEX'] = [
-				'cu_changes' => $this->xfor ? 'cuc_xff_hex_time' : 'cuc_ip_hex_time'
+		# When reading new, only select results from cu_changes that are
+		# for read new (defined as those with cuc_only_for_read_old set to 0).
+		if ( $this->eventTableReadNew ) {
+			$queryInfo['conds']['cuc_only_for_read_old'] = 0;
+		}
+		return $queryInfo;
+	}
+
+	/** @inheritDoc */
+	protected function getQueryInfoForCuLogEvent(): array {
+		$commentQuery = $this->commentStore->getJoin( 'log_comment' );
+		$queryInfo = [
+			'fields' => [
+				'timestamp' => 'cule_timestamp',
+				'title' => 'log_title',
+				'page_id' => 'log_page',
+				'namespace' => 'log_namespace',
+				'ip' => 'cule_ip',
+				'ip_hex' => 'cule_ip_hex',
+				'xff' => 'cule_xff',
+				'xff_hex' => 'cule_xff_hex',
+				'agent' => 'cule_agent',
+				'user' => 'actor_user',
+				'user_text' => 'actor_name',
+				'comment_text',
+				'comment_data',
+				'log_type' => 'log_type',
+				'log_action' => 'log_action',
+				'log_params' => 'log_params',
+				'log_id' => 'cule_log_id',
+			],
+			'tables' => [
+				'cu_log_event', 'logging_cule_log_id' => 'logging', 'actor_log_actor' => 'actor'
+			] + $commentQuery['tables'],
+			'conds' => [],
+			'join_conds' => [
+				'logging_cule_log_id' => [ 'JOIN', 'logging_cule_log_id.log_id=cule_log_id' ],
+				'actor_log_actor' => [ 'JOIN', 'actor_log_actor.actor_id=log_actor' ],
+			] + $commentQuery['joins'],
+			'options' => [],
+		];
+		if ( $this->mDb->getType() == 'postgres' ) {
+			# On postgres the cuc_type type is a smallint.
+			$queryInfo['fields'] += [
+				'type' => 'CAST(' . RC_LOG . ' AS smallint)'
 			];
-			$ipConds = self::getIpConds( $this->mDb, $this->target->getName(), $this->xfor );
-			if ( $ipConds ) {
-				$queryInfo['conds'] = array_merge( $queryInfo['conds'], $ipConds );
-			}
+		} else {
+			# Other DBs can handle converting RC_LOG to the
+			# correct type.
+			$queryInfo['fields'] += [
+				'type' => RC_LOG
+			];
+		}
+		return $queryInfo;
+	}
+
+	/** @inheritDoc */
+	protected function getQueryInfoForCuPrivateEvent(): array {
+		$commentQuery = $this->commentStore->getJoin( 'cupe_comment' );
+		$queryInfo = [
+			'fields' => [
+				'timestamp' => 'cupe_timestamp',
+				'title' => 'cupe_title',
+				'page_id' => 'cupe_page',
+				'namespace' => 'cupe_namespace',
+				'ip' => 'cupe_ip',
+				'ip_hex' => 'cupe_ip_hex',
+				'xff' => 'cupe_xff',
+				'xff_hex' => 'cupe_xff_hex',
+				'agent' => 'cupe_agent',
+				'user' => 'actor_user',
+				'user_text' => 'actor_name',
+				'comment_text',
+				'comment_data',
+				'log_type' => 'cupe_log_type',
+				'log_action' => 'cupe_log_action',
+				'log_params' => 'cupe_params'
+			],
+			'tables' => [ 'cu_private_event', 'actor_cupe_actor' => 'actor' ] + $commentQuery['tables'],
+			'conds' => [],
+			'join_conds' => [
+				'actor_cupe_actor' => [ 'JOIN', 'actor_cupe_actor.actor_id=cupe_actor' ]
+			] + $commentQuery['joins'],
+			'options' => [],
+		];
+		if ( $this->mDb->getType() == 'postgres' ) {
+			# On postgres the cuc_type type is a smallint.
+			$queryInfo['fields'] += [
+				'type' => 'CAST(' . RC_LOG . ' AS smallint)'
+			];
+		} else {
+			# Other DBs can handle converting RC_LOG to the
+			# correct type.
+			$queryInfo['fields'] += [
+				'type' => RC_LOG
+			];
 		}
 		return $queryInfo;
 	}

src/CheckUser/Pagers/AbstractCheckUserPager.php
@@ -8,10 +8,12 @@ use ExtensionRegistry;
 use Html;
 use HtmlArmor;
 use IContextSource;
+use LogicException;
 use MediaWiki\Block\AbstractBlock;
 use MediaWiki\Block\DatabaseBlock;
 use MediaWiki\CheckUser\CheckUser\CheckUserPagerNavigationBuilder;
 use MediaWiki\CheckUser\CheckUser\Widgets\HTMLFieldsetCheckUser;
+use MediaWiki\CheckUser\CheckUserQueryInterface;
 use MediaWiki\CheckUser\Services\CheckUserLogService;
 use MediaWiki\CheckUser\Services\CheckUserUnionSelectQueryBuilderFactory;
 use MediaWiki\CheckUser\Services\TokenQueryManager;
@@ -33,11 +35,13 @@ use SpecialPage;
 use TitleValue;
 use UserGroupMembership;
 use Wikimedia\IPUtils;
+use Wikimedia\Rdbms\Database\DbQuoter;
+use Wikimedia\Rdbms\FakeResultWrapper;
 use Wikimedia\Rdbms\ILoadBalancer;
-use Wikimedia\Rdbms\IReadableDatabase;
+use Wikimedia\Rdbms\IResultWrapper;
 use Wikimedia\Rdbms\SelectQueryBuilder;
 
-abstract class AbstractCheckUserPager extends RangeChronologicalPager {
+abstract class AbstractCheckUserPager extends RangeChronologicalPager implements CheckUserQueryInterface {
 
 	/**
 	 * Form fields that when paging should be set and managed
@@ -71,6 +75,9 @@ abstract class AbstractCheckUserPager extends RangeChronologicalPager {
 	protected FormOptions $opts;
 
 	protected UserIdentity $target;
+
+	/** @var bool Should Special:CheckUser read from the new event tables. */
+	protected bool $eventTableReadNew;
 
 	/**
 	 * @var string one of the SpecialCheckUser::SUBTYPE_... constants used by this abstract pager
@@ -154,6 +161,9 @@ abstract class AbstractCheckUserPager extends RangeChronologicalPager {
 
 		$this->mLimitsShown = array_map( 'ceil', $this->mLimitsShown );
 		$this->mLimitsShown = array_unique( $this->mLimitsShown );
+		$this->eventTableReadNew = boolval(
+			$this->getConfig()->get( 'CheckUserEventTablesMigrationStage' ) & SCHEMA_COMPAT_READ_NEW
+		);
 
 		$this->userGroupManager = $userGroupManager;
 		$this->centralIdLookup = $centralIdLookup;
@@ -184,22 +194,22 @@ abstract class AbstractCheckUserPager extends RangeChronologicalPager {
 			);
 		}
 
-		$this->getPeriodCondition();
+		$this->setPeriodCondition();
 	}
 
 	/**
-	 * Get the cutoff timestamp condition for the query
-	 *
-	 * @return array the cutoff timestamp query condition
+	 * Generate the cutoff timestamp condition for the query
 	 */
-	protected function getPeriodCondition() {
+	protected function setPeriodCondition(): void {
 		$period = $this->opts->getValue( 'period' );
 		if ( $period ) {
 			$cutoffTime = MWTimestamp::getInstance();
 			$cutoffTime->timestamp->setTime( 0, 0 )->modify( "-$period day" );
-			return $this->getDateRangeCond( $cutoffTime->getTimestamp(), '' );
+			// Call to RangeChronologicalPager::getDateRangeCond sets
+			// $this->startOffset to the $cutoffTime. The call does
+			// not set $this->endOffset as the empty string is provided.
+			$this->getDateRangeCond( $cutoffTime->getTimestamp(), '' );
 		}
-		return [];
 	}
 
 	/**
@@ -490,13 +500,17 @@ abstract class AbstractCheckUserPager extends RangeChronologicalPager {
 	/**
 	 * Get the WHERE conditions for an IP address / range, optionally as a XFF.
 	 *
-	 * @param IReadableDatabase $dbr
+	 * @param DbQuoter $quoter A DB quoter, which can be a IReadableDatabase instance if convenient.
 	 * @param string $target an IP address or CIDR range
-	 * @param string|bool $xfor
+	 * @param bool $xfor True if searching on XFF IPs by IP address / range
+	 * @param string $table The table which will be used in the query these WHERE conditions
+	 * are used (array of valid options in self::RESULT_TABLES).
 	 * @return array|false array for valid conditions, false if invalid
 	 */
-	public static function getIpConds( IReadableDatabase $dbr, string $target, $xfor = false ) {
-		$type = $xfor ? 'xff' : 'ip';
+	public static function getIpConds(
+		DbQuoter $quoter, string $target, bool $xfor = false, string $table = self::CHANGES_TABLE
+	) {
+		$columnName = self::getIpHexColumn( $xfor, $table );
 
 		if ( !self::isValidRange( $target ) ) {
 			return false;
@@ -504,31 +518,75 @@ abstract class AbstractCheckUserPager extends RangeChronologicalPager {
 
 		if ( IPUtils::isValidRange( $target ) ) {
 			list( $start, $end ) = IPUtils::parseRange( $target );
-			return [ 'cuc_' . $type . '_hex BETWEEN ' . $dbr->addQuotes( $start ) .
-				' AND ' . $dbr->addQuotes( $end ) ];
+			return [ $columnName . ' BETWEEN ' . $quoter->addQuotes( $start ) .
+				' AND ' . $quoter->addQuotes( $end ) ];
 		} elseif ( IPUtils::isValid( $target ) ) {
-			return [ "cuc_{$type}_hex" => IPUtils::toHex( $target ) ];
+			return [ $columnName => IPUtils::toHex( $target ) ];
 		}
 		// invalid IP
 		return false;
 	}
 
-	/** @inheritDoc */
-	public function getIndexField() {
-		if ( $this->getConfig()->get( 'CheckUserEventTablesMigrationStage' ) & SCHEMA_COMPAT_READ_OLD ) {
-			return 'cuc_timestamp';
+	/**
+	 * Gets the column name for the IP hex column based
+	 * on the value for $xfor and a given $table.
+	 *
+	 * @param bool $xfor Whether the IPs being searched through are XFF IPs.
+	 * @param string $table The table selecting results from (array of valid
+	 * options in self::RESULT_TABLES).
+	 * @return string
+	 */
+	private static function getIpHexColumn( bool $xfor, string $table ): string {
+		$type = $xfor ? 'xff' : 'ip';
+		return self::RESULT_TABLE_TO_PREFIX[$table] . $type . '_hex';
+	}
+
+	/**
+	 * Gets the name for the index for a given table.
+	 *
+	 * note: When SCHEMA_COMPAT_READ_NEW is set, the query will not use an index
+	 * on the values of `cuc_only_for_read_old`.
+	 * That shouldn't result in a significant performance drop, and this is a
+	 * temporary situation until the temporary column is removed after the
+	 * migration is complete.
+	 *
+	 * @param string $table The table this index should apply to (list of valid options
+	 *   in self::RESULT_TABLES).
+	 * @return string
+	 */
+	protected function getIndexName( string $table ): string {
+		if ( $this->xfor === null ) {
+			return self::RESULT_TABLE_TO_PREFIX[$table] . 'actor_ip_time';
 		} else {
-			return 'timestamp';
+			$type = $this->xfor ? 'xff' : 'ip';
+			return self::RESULT_TABLE_TO_PREFIX[$table] . $type . '_hex_time';
 		}
 	}
 
 	/** @inheritDoc */
-	public function getTimestampField() {
-		if ( $this->getConfig()->get( 'CheckUserEventTablesMigrationStage' ) & SCHEMA_COMPAT_READ_OLD ) {
-			return 'cuc_timestamp';
-		} else {
+	public function getIndexField(): string {
+		return 'timestamp';
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * If the timestamp field is null, then the caller is either from outside
+	 * CheckUser code or is not aware of what table a row comes from when
+	 * reading results. In both cases returning "timestamp" is best as the
+	 * queries alias the timestamp field to this name.
+	 *
+	 * If a caller uses the result of this method when $table is null as
+	 * a column name in the WHERE conditions, the query will fail.
+	 *
+	 * @param string|null $table The table name. If null, returns the string "timestamp".
+	 * @return string The timestamp field.
+	 */
+	public function getTimestampField( ?string $table = null ): string {
+		if ( $table === null ) {
 			return 'timestamp';
 		}
+		return self::RESULT_TABLE_TO_PREFIX[$table] . 'timestamp';
 	}
 
 	/** @inheritDoc */
@@ -613,5 +671,166 @@ abstract class AbstractCheckUserPager extends RangeChronologicalPager {
 		} else {
 			return '';
 		}
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @param string|null $table One of the tables in CheckUserQueryInterface::RESULT_TABLES.
+	 *   If null when reading new, this will throw an Exception.
+	 * @throws LogicException if $table is null a LogicException is thrown as ::getQueryInfo
+	 * must have this information to return the correct query info.
+	 */
+	abstract public function getQueryInfo( ?string $table = null );
+
+	/**
+	 * Get the query info specific to cu_changes that will
+	 * be extended with table independent query information
+	 * (such as a actor_name WHERE condition). This
+	 * method should only be called by the ::getQueryInfo
+	 * implementation.
+	 *
+	 * @return array The query info specific to cu_changes
+	 */
+	abstract protected function getQueryInfoForCuChanges(): array;
+
+	/**
+	 * Get the query info specific to cu_log_event that will
+	 * be extended with table independent query information
+	 * (such as a actor_name WHERE condition). This
+	 * method should only be called by the ::getQueryInfo
+	 * implementation.
+	 *
+	 * @return array The query info specific to cu_log_event
+	 */
+	abstract protected function getQueryInfoForCuLogEvent(): array;
+
+	/**
+	 * Get the query info specific to cu_private_event that will
+	 * be extended with table independent query information
+	 * (such as a actor_name WHERE condition). This
+	 * method should only be called by the ::getQueryInfo
+	 * implementation.
+	 *
+	 * @return array The query info specific to cu_private_event
+	 */
+	abstract protected function getQueryInfoForCuPrivateEvent(): array;
+
+	/** @inheritDoc */
+	public function reallyDoQuery( $offset, $limit, $order ): IResultWrapper {
+		$results = [];
+		// Run the three SQL queries for each results table.
+		foreach ( $this->buildQueryInfo( $offset, $limit, $order ) as $queryInfo ) {
+			[ $tables, $fields, $conds, $fname, $options, $join_conds ] = $queryInfo;
+			$indexField = $this->getIndexField();
+
+			$queryResult = $this->mDb->select( $tables, $fields, $conds, $fname, $options, $join_conds );
+			// Expand the result set into an array, with the key as the timestamp and
+			// value as an array of rows that have this timestamp.
+			foreach ( $queryResult as $row ) {
+				// Use an array of rows as two given rows could have the same
+				// timestamp value.
+				if ( isset( $results[$row->$indexField] ) ) {
+					$results[$row->$indexField][] = $row;
+				} else {
+					$results[$row->$indexField] = [ $row ];
+				}
+			}
+		}
+		// Make a new result wrapper that combines the results by ordering all
+		// by their timestamp and then returning the first $limit of the items.
+		if ( $order === self::QUERY_DESCENDING ) {
+			krsort( $results );
+		} else {
+			ksort( $results );
+		}
+		// Can remove the keys now that the results have been sorted.
+		$results = array_values( $results );
+		// Flatten the array. If for a given timestamp more than row is present,
+		// from the same table then this method will keep the order in which
+		// they were returned by IDatabase::select. If the rows come from
+		// different tables, the rows will be ordered by table in the order
+		// the tables are defined in self::RESULT_TABLES.
+		$flattened_results = [];
+		array_walk_recursive( $results, static function ( $value ) use ( &$flattened_results ) {
+			$flattened_results[] = $value;
+		} );
+		// Apply the limit to the results.
+		$flattened_results = array_slice( $flattened_results, 0, $limit );
+
+		// Return the generated data as a FakeResultWrapper.
+		return new FakeResultWrapper( $flattened_results );
+	}
+
+	/**
+	 * Builds the query information. This is the same code as written in
+	 * IndexPager::buildQueryInfo, ReverseChronologicalPager::buildQueryInfo
+	 * and RangeChronologicalPager::buildQueryInfo, but modifies it to
+	 * provide ::getQueryInfo with the arguments passed to this method.
+	 *
+	 * @inheritDoc
+	 */
+	public function buildQueryInfo( $offset, $limit, $order ): array {
+		// Copied, with modification, from IndexPager::buildQueryInfo
+		$fname = __METHOD__ . ' (' . $this->getSqlComment() . ')';
+		$queryInfo = [];
+		# Select data from all three tables when reading new, and only cu_changes when reading old.
+		$resultTables = self::RESULT_TABLES;
+		if ( $this->getConfig()->get( 'CheckUserEventTablesMigrationStage' ) & SCHEMA_COMPAT_READ_OLD ) {
+			$resultTables = [ self::CHANGES_TABLE ];
+		}
+		foreach ( $resultTables as $table ) {
+			$info = $this->getQueryInfo( $table );
+			$tables = $info['tables'];
+			$fields = $info['fields'];
+			$conds = $info['conds'] ?? [];
+			$options = $info['options'] ?? [];
+			$join_conds = $info['join_conds'] ?? [];
+			$indexColumns = (array)$this->mIndexField;
+			$sortColumns = array_merge( $indexColumns, $this->mExtraSortFields );
+
+			if ( $order === self::QUERY_ASCENDING ) {
+				$options['ORDER BY'] = $sortColumns;
+				$operator = $this->mIncludeOffset ? '>=' : '>';
+			} else {
+				$orderBy = [];
+				foreach ( $sortColumns as $col ) {
+					$orderBy[] = $col . ' DESC';
+				}
+				$options['ORDER BY'] = $orderBy;
+				$operator = $this->mIncludeOffset ? '<=' : '<';
+			}
+			if ( $offset ) {
+				# From IndexPager
+				$offsets = explode( '|', $offset, count( $indexColumns ) );
+				$indexColumns = array_slice( $indexColumns, 0, count( $offsets ) );
+				# Replace 'timestamp' with the timestamp column name for the given table.
+				$timestampField = $this->getTimestampField( $table );
+				$indexColumns = array_map( static function ( $value ) use ( $timestampField ) {
+					return $value === 'timestamp' ? $timestampField : $value;
+				}, $indexColumns );
+				# From IndexPager
+				$conds[] = $this->mDb->buildComparison( $operator, array_combine( $indexColumns, $offsets ) );
+			}
+			$options['LIMIT'] = intval( $limit );
+
+			// Copied from ReverseChronologicalPager::buildQueryInfo
+			if ( $this->endOffset ) {
+				$conds[] = $this->mDb->buildComparison(
+					'<', [ $this->getTimestampField( $table ) => $this->endOffset ]
+				);
+			}
+
+			// Copied from RangeChronologicalPager::buildQueryInfo
+			if ( $this->startOffset ) {
+				$conds[] = $this->mDb->buildComparison(
+					'>=', [ $this->getTimestampField( $table ) => $this->startOffset ]
+				);
+			}
+			// Add the data that would normally be returned by this method to an array
+			// so that it can be returned for all three tables at once.
+			$queryInfo[$table] = [ $tables, $fields, $conds, $fname, $options, $join_conds ];
+		}
+		return $queryInfo;
 	}
 }

src/CheckUser/Pagers/CheckUserGetIPsPager.php
@@ -5,6 +5,7 @@ namespace MediaWiki\CheckUser\CheckUser\Pagers;
 use ActorMigration;
 use CentralIdLookup;
 use IContextSource;
+use InvalidArgumentException;
 use MediaWiki\Block\DatabaseBlock;
 use MediaWiki\CheckUser\CheckUser\SpecialCheckUser;
 use MediaWiki\CheckUser\Services\CheckUserLogService;
@@ -142,7 +143,9 @@ class CheckUserGetIPsPager extends AbstractCheckUserPager {
 		}
 		// We are only using startOffset for the period feature.
 		if ( $this->startOffset ) {
-			$conds[] = $this->mDb->buildComparison( '>=', [ $this->getTimestampField() => $this->startOffset ] );
+			$conds[] = $this->mDb->buildComparison(
+				'>=', [ $this->getTimestampField( self::CHANGES_TABLE ) => $this->startOffset ]
+			);
 		}
 
 		// Get counts for this IP / IP range
@@ -176,7 +179,12 @@ class CheckUserGetIPsPager extends AbstractCheckUserPager {
 	}
 
 	/** @inheritDoc */
-	public function getQueryInfo(): array {
+	public function getQueryInfo( ?string $table = null ): array {
+		if ( $table !== self::CHANGES_TABLE ) {
+			throw new InvalidArgumentException(
+				"This ::getQueryInfo method has not implemented read new support."
+			);
+		}
 		return [
 			'fields' => [
 				'ip' => 'cuc_ip',
@@ -196,7 +204,28 @@ class CheckUserGetIPsPager extends AbstractCheckUserPager {
 	}
 
 	/** @inheritDoc */
-	public function getIndexField() {
+	protected function getQueryInfoForCuChanges(): array {
+		// No read new support yet, so return empty array to be compatible with definition
+		// in AbstractCheckUserPager
+		return [];
+	}
+
+	/** @inheritDoc */
+	protected function getQueryInfoForCuLogEvent(): array {
+		// No read new support yet, so return empty array to be compatible with definition
+		// in AbstractCheckUserPager
+		return [];
+	}
+
+	/** @inheritDoc */
+	protected function getQueryInfoForCuPrivateEvent(): array {
+		// No read new support yet, so return empty array to be compatible with definition
+		// in AbstractCheckUserPager
+		return [];
+	}
+
+	/** @inheritDoc */
+	public function getIndexField(): string {
 		return 'last';
 	}
 

commit 532383000e6c27b548495b11996f69cbd47fda75

Fix read old support broken in 022e4ffc915af7ab39a4cee534595b9d4b9d6018.

src/CheckUser/Pagers/CheckUserGetEditsPager.php
@@ -491,9 +491,7 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 				'user' => 'actor_user',
 				'user_text' => 'actor_name',
 				// Needed for rows with cuc_type as RC_LOG.
-				'comment_text',
-				'comment_data',
-			],
+			] + $commentQuery['fields'],
 			'tables' => [ 'cu_changes', 'actor_cuc_user' => 'actor' ] + $commentQuery['tables'],
 			'conds' => [],
 			'join_conds' => [
@@ -533,14 +531,12 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 				'agent' => 'cule_agent',
 				'user' => 'actor_user',
 				'user_text' => 'actor_name',
-				'comment_text',
-				'comment_data',
 				'log_type' => 'log_type',
 				'log_action' => 'log_action',
 				'log_params' => 'log_params',
 				'log_deleted' => 'log_deleted',
 				'log_id' => 'cule_log_id',
-			],
+			] + $commentQuery['fields'],
 			'tables' => [
 				'cu_log_event', 'logging_cule_log_id' => 'logging', 'actor_log_actor' => 'actor'
 			] + $commentQuery['tables'],
@@ -590,14 +586,12 @@ class CheckUserGetEditsPager extends AbstractCheckUserPager {
 				'agent' => 'cupe_agent',
 				'user' => 'actor_user',
 				'user_text' => 'actor_name',
-				'comment_text',
-				'comment_data',
 				'log_type' => 'cupe_log_type',
 				'log_action' => 'cupe_log_action',
 				'log_params' => 'cupe_params',
 				// cu_private_event log events cannot be deleted or suppressed.
 				'log_deleted' => 0,
-			],
+			] + $commentQuery['fields'],
 			'tables' => [ 'cu_private_event', 'actor_cupe_actor' => 'actor' ] + $commentQuery['tables'],
 			'conds' => [],
 			'join_conds' => [